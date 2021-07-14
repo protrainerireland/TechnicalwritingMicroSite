5fb5a0845914d047c930493cfdb9dcbc9697e657
protrainerireland <info@professional.ie>
2021-07-14 10:48:59 +0100

    added makeSection filter to eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,13 @@
 module.exports = function(config) {
+
+    config.addFilter("makeSection", function(content) {
+        return `<div>${content.title}</div>`;
+    });
+
     return {
         dir: {
             input:'./src', 
             output: './build'
         }
-    }
-}
+    };
+};
