123a06400e1553e8a5b9962d400a4c34fb2396dc
CALUMONEILL <120475986@umail.ucc.ie>
2022-05-16 16:15:21 +0100

    Commit before pulling from master

--- src/_includes/makeSection.js
+++ src/_includes/makeSection.js
@@ -188,6 +188,7 @@ module.exports = {
                 break; bv
             case "keywordtext":
 
+                list = `<ul>${section.content.list.map(item=>`<li>${item}</li>`).join("")}</ul>`;
 
                 //let defaultTemplate = `<div>${site.searchKeywords.join(" ")}</div>`;
                 let defaultTemplate = `${site.searchKeywords[0]} is one of the most popular coding languages in the world. The job market for ${ site.searchKeywords[0] } developers is robust and consistent, making it a great programming language to learn. Our courses can give you the skills you need to work effectively with ${ site.searchKeywords[0] }.  Why attend one of our ${ site.searchKeywords[0] } courses? If you're interested working effectively with ${ site.searchKeywords[0] } to benefit your organisation and to get an edge over your competitors, or simply to learn a highly lucrative skill then you should consider our ${ site.searchKeywords[0] } training course.</p>`
@@ -208,7 +209,9 @@ module.exports = {
                         data-aos-anchor="#${section.id}"
                         >
                         <p class="about-title">${ section.content.title }</p>
-                        <p>${ defaultTemplate }</p>
+                        <p>${ section.content.text1 }</p>
+                        <p>${ section.content.text2 }</p>
+                        ${list}
                         </div>`;
                     if (section.image) {
                         html += `<div class="col-md-6" 
@@ -236,6 +239,7 @@ module.exports = {
                     <div class="row">
                         <div class="col-md-6 about" data-aos="fade-left" ${ animationDelay } ${animationOffset}>
                         <p class="about-title">${ section.content.title }</p>
+                        
                         ${list}
                         </div>
                         <div class="col-md-6" data-aos="fade-right" ${ animationDelay } ${animationOffset}>
@@ -322,6 +326,38 @@ module.exports = {
                             </div>
                         </section>`;
                 break;
+
+                case "summary":
+
+                    list = `<ul>${section.content.list.map(item=>`<li>${item}</li>`).join("")}</ul>`;
+                    
+                    html = `<section id="${section.id}" class="section">
+                        <div class="container">
+                        <h3 class="title text-center">${ section.title }</h3>
+                        <div class="row ${section.imagePosition=="left" ? "flex-row-reverse" :""}">
+                            <div class="${section.image ? 'col-md-6' : 'col-md-12'} about"
+                            data-aos="fade-left" ${ animationDelay } ${animationOffset}
+                            data-aos-anchor="#${section.id}"
+                            >
+                            <p class="about-title">${ section.content.title }</p>
+                            <p>${ section.content.text1 }</p>
+                            <p>${ section.content.text2 }</p>
+                            ${list}
+                            
+                            </div>`;
+                        if (section.image) {
+                            html += `<div class="col-md-6"
+                                    data-aos="fade-right" ${ animationDelay }  ${animationOffset}
+                                    data-aos-anchor="#${section.id}"
+                            >
+                                        <img src="${ section.image }" class="img-fluid" alt="">
+                                    </div>`;
+                        }    
+        
+                    html += `</div>
+                        </div>
+                    </section>`;
+                    break;
 
             case 'pricing':
 
--- src/index.md
+++ src/index.md
@@ -1,6 +1,6 @@
 ---
 layout: homepage_layout
-title: Javascript Training Courses | Dublin Cork Limerick Ireland
+title: Report Writing Training Courses | Dublin Cork Limerick Ireland
 page:
     type: home
 ---
