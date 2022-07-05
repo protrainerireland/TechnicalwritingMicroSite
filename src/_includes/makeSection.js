56b0e073576b1225cb0050a38736970ce4d866ae
CALUMONEILL <120475986@umail.ucc.ie>
2022-07-05 11:41:22 +0100

    Redesigned how section text is stored and accessed

--- src/_includes/makeSection.js
+++ src/_includes/makeSection.js
@@ -126,9 +126,6 @@ module.exports = {
                 html = ``;
                 break;
 
-                 break;
-
-
             case "embeddedsvg":
                 html = `<section id="${section.id}" class="section">
                             <div class="container">
@@ -142,7 +139,6 @@ module.exports = {
                         </section>
                 `;
                 break;
-
 
             case "inlinesvg": 
 
@@ -164,6 +160,7 @@ module.exports = {
                         </section>
                 `;
                 break;
+
             case 'banner':
 
                 html = `<section class="section banner">
@@ -182,6 +179,7 @@ module.exports = {
                         <img src="${ section.background }" class="bottom-img" alt="">
                     </section>`;
                 break;
+
             case "grid":
                 html = `<section id="${section.id}" class="section">
                             <div class="container text-center">
@@ -201,6 +199,7 @@ module.exports = {
                         </div>
                     </section>`;
                 break; bv
+
             case "keywordtext":
 
                 try {
@@ -212,29 +211,33 @@ module.exports = {
 
                 }
 
-                //let defaultTemplate = `<div>${site.searchKeywords.join(" ")}</div>`;
-                let defaultTemplate = `${site.searchKeywords[0]} is one of the most popular coding languages in the world. The job market for ${ site.searchKeywords[0] } developers is robust and consistent, making it a great programming language to learn. Our courses can give you the skills you need to work effectively with ${ site.searchKeywords[0] }.  Why attend one of our ${ site.searchKeywords[0] } courses? If you're interested working effectively with ${ site.searchKeywords[0] } to benefit your organisation and to get an edge over your competitors, or simply to learn a highly lucrative skill then you should consider our ${ site.searchKeywords[0] } training course.</p>`
-                
-                if (section.image) {
-                    //..tbd
+                try {
+                    text = `<ul>${section.content.text.map(item=>`<p>${item}</p>`).join("")}</ul>`;
+
+                } catch(error) {
+                    text = `<ul></ul>`;
+                    console.log(`***** metadata error section.content.list is missing - keywordtext *****`);  
+
                 }
 
-                if (section.left) {
-                    // tbd
-                }
+                let defaultTemplate = `${site.searchKeywords[0]} is one of the most popular coding languages in the world. The job market for ${ site.searchKeywords[0] } developers is robust and consistent, making it a great programming language to learn. Our courses can give you the skills you need to work effectively with ${ site.searchKeywords[0] }.  Why attend one of our ${ site.searchKeywords[0] } courses? If you're interested working effectively with ${ site.searchKeywords[0] } to benefit your organisation and to get an edge over your competitors, or simply to learn a highly lucrative skill then you should consider our ${ site.searchKeywords[0] } training course.</p>`
+                
                 html = `<section id="${section.id}" class="section">
                     <div class="container">
                     <h3 class="title text-center">${ section.title }</h3>
                     <div class="row ${section.imagePosition=="left" ? "flex-row-reverse" :""}">
                         <div class="${section.image ? 'col-md-6' : 'col-md-12'} about" 
                         data-aos="fade-left" ${ animationDelay } ${animationOffset}
-                        data-aos-anchor="#${section.id}"
-                        >
+                        data-aos-anchor="#${section.id}">
+                        
                         <p class="about-title">${ section.content.title }</p>
-                        <p>${ section.content.text1 }</p>
-                        <p>${ section.content.text2 }</p>
+                        
+                        ${text}
+                        
                         ${list}
+
                         </div>`;
+
                     if (section.image) {
                         html += `<div class="col-md-6" 
                                 data-aos="fade-right" ${ animationDelay }  ${animationOffset}
@@ -248,71 +251,6 @@ module.exports = {
                     </div>
                 </section>`;
                 break;
-
-            case "animatedtwocolumns":
-                try {
-                    list = `<ul>${section.content.list.map(item=>`<li>${item}</li>`).join("")}</ul>`;
-                } catch(error) {
-                    list = `<ul></ul>`;
-                    console.log(`***** metadata error section.content.list is missing - animatedtwocolumns *****`);
-                    
-                }
-
-                html = `<section id="${section.id}" class="section">
-                    <div class="container">
-                    <h3 class="title text-center">${ section.title }</h3>
-                    <div class="row">
-                        <div class="col-md-6 about" data-aos="fade-left" ${ animationDelay } ${animationOffset}>
-                        <p class="about-title">${ section.content.title }</p>
-                        
-                        ${list}
-                        </div>
-                        <div class="col-md-6" data-aos="fade-right" ${ animationDelay } ${animationOffset}>
-                            <img src="${ section.content.image }" class="img-fluid" alt="">
-                        </div>
-                    </div>
-                    </div>
-                </section>`;
-
-
-                html = `<section class="pb-0" id="${section.id}">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-6" data-aos="fade-left" ${animationDelay} ${animationOffset}>
-                                <div class="heading-area">
-                                    <span class="sub-title"></span>
-                                    <h4 class="">
-                                        <span class="alt-color js-rotating">
-                                            Im'going to start a business BUT I'm not going to make any real effort to see if it will be profitable or even survive!,
-                                            Finance! I leave that to the accontants<br><br><br>,
-                                            I'll pick up what I need to know once I get started!<br><br><br>
-                                        </span>
-                                    </h4>
-                                    <p class="para">
-                                        Sound ludicrous? We think so too but it is the way many entrepreneurs approach business and one  of the main reasons why so many new ventures fail so quickly.
-            
-                                        We developed www.dolearnfinance.com specifically to increase financial skills in the start-up sector by providing an attractive way for entrepreneurs to rapidly acquire them and be ready to engage confidently with providers of funds.
-            
-                                        The unique feature of this site is the complete alignment of learning and applying. The dedicated modelling applications which create an innovative learn-by-doing programme are then used to follow through and build business models and financial projections for real business start-ups.
-                                    </p>
-            
-                                </div>
-                            </div>
-                            <div class="col-lg-6" data-aos="fade-right" ${animationDelay} ${animationOffset}>
-                                <div class="half-img mt-5 pt-4 mt-lg-0 pt-lg-0">
-                                    <img alt="vector" src="${section.content.image}">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>                
-                `;
-
-
-
-
-                break;
-                
 
             case "listwithimage":
                 try {
@@ -337,6 +275,7 @@ module.exports = {
                     </div>
                 </section>`;
                 break;
+
             case 'quotes':
                 let quotes = section.content.quotes.map((quote, index) => `<div class="col-md-5 testimonials" data-aos="fade-left" data-aos-delay="${ site.animation.delay * (index + 1) }" ${animationOffset}>
                     <p class="feedback">"${quote.text}"</p>
@@ -362,6 +301,15 @@ module.exports = {
                         list = `<ul></ul>`;
                         console.log(`***** metadata error section.content.list is missing - summary *****`);
                     }
+
+                    try {
+                        text = `<ul>${section.content.text.map(item=>`<p>${item}</p>`).join("")}</ul>`;
+    
+                    } catch(error) {
+                        text = `<ul></ul>`;
+                        console.log(`***** metadata error section.content.list is missing - keywordtext *****`);  
+    
+                    }
                     
                     html = `<section id="${section.id}" class="section">
                         <div class="container">
@@ -372,8 +320,9 @@ module.exports = {
                             data-aos-anchor="#${section.id}"
                             >
                             <p class="about-title">${ section.content.title }</p>
-                            <p>${ section.content.text1 }</p>
-                            <p>${ section.content.text2 }</p>
+                            
+                            ${text}
+                            
                             ${list}
                             
                             </div>`;
